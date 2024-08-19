ecbf19ca06fcefee94a6f5a8fd26839ba6d26cf8
AliMurad-codes <ranaali0601@gmail.com>
2024-08-19 20:53:57 +0500

    h

--- Module3/Algorithms.py
+++ Module3/Algorithms.py
@@ -22,3 +22,31 @@ Destination: C
 '''
 # Actual function call
 hanoi(disks, 'A', 'B', 'C')
+
+# recurtion
+def string_reverse(str):
+    if len(str) == 0:
+        return str
+    else:
+         return string_reverse(str[1:]) + str[0]
+
+str = "reversal"
+reverse = string_reverse(str)
+print(reverse)
+
+# without recursion solve the same problem:
+a = "reversal"
+b = a[::-1]
+print(b)
+
+
+# mapping:
+
+menu = ["espresso", "mocha", "latte", "cappuccino", "cortado", "americano"]
+def find_coffee(coffee):
+    if coffee [0] == 'c':
+        return coffee
+map_coffee = map (find_coffee, menu)
+print(map_coffee)
+for x in map_coffee:
+    print(x)
