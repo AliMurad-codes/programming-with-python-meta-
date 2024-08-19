cccf81a701bf40d9e54e1363ddf7e5147e5d20a0
AliMurad-codes <ranaali0601@gmail.com>
2024-08-19 22:27:39 +0500

    n

--- Module3/comprehension.py
+++ Module3/comprehension.py
@@ -1,3 +1,4 @@
+# List comprehension
 data = [2,3,5,7,11,13,17,19,23,29,31]
 
 # Ex1: List comprehension: updating the same list
@@ -19,3 +20,56 @@ print("Divisible by four minus one: ", fourxsub)
 # Ex5: Using range function:
 nines = [x for x in range(100) if x%9 == 0]
 print("Nines: ", nines)
+
+
+# List comprehension:
+data = [x+3 for x in data]
+
+# Regular for loop:
+for x in range(len(data)):
+    data[x] = data[x] + 3
+
+
+# Dictionary comprehension:
+# Using range() function and no input list
+usingrange = {x:x*2 for x in range(12)}
+print("Using range(): ",usingrange)
+
+# Lists
+months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+number = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+# Using one input list
+numdict = {x:x**2 for x in number}
+print("Using one input list to create dict: ", numdict)
+
+# Using two input lists
+months_dict = {key:value for (key, value) in zip(number, months)}
+print("Using two lists: ", months_dict)
+
+# Set comprehension
+set_a = {x for x in range(10,20) if x not in [12,14,16]}
+print(set_a)
+
+# Generator comprehension
+data = [2,3,5,7,11,13,17,19,23,29,31]
+gen_obj = (x for x in data)
+print(gen_obj)
+print(type(gen_obj))
+for items in gen_obj:
+    print(items, end = " ")
+
+def sum(n):
+   if n == 1:
+       return 0
+   return n + sum(n-1)
+
+a = sum(5)
+print(a)
+
+numbers = [15, 30, 47, 82, 95]
+def lesser(numbers):
+   return numbers < 50
+
+small = list(filter(lesser, numbers))
+print(small)
