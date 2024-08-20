18f9d588662765ca5bbf9ca1acafecea66b96fe9
AliMurad-codes <ranaali0601@gmail.com>
2024-08-20 12:59:15 +0500

    commit

--- Module3/list,dict.py
+++ Module3/list,dict.py
@@ -22,14 +22,14 @@ print(get_employee(12458))
 
 
 
-In this code, employee_list is a list of tuples. The code runs well and will return the user Paul, as its ID, 12458, is matched. The challenge comes when the list gets bigger.
+# In this code, employee_list is a list of tuples.
+# The code runs well and will return the user Paul, as its ID, 12458, is matched. The challenge comes when the list gets bigger.
+# Instead of two employees, you may have 2000 or even 20,000.
+# The code will have to iterate over the list sequentially until the number is matched.
+# You could optimize the code to split the search, but even with this,
+# it still lacks in performance in comparison to other data structures, such as the dictionary.
 
-Instead of two employees, you may have 2000 or even 20,000. The code will have to iterate over the list sequentially until the number is matched.
-
-You could optimize the code to split the search, but even with this, it still lacks in performance in comparison to other data structures, such as the dictionary.
-
-
- employee_dict = {
+employee_dict = {
     12345: {
         "id": "12345",
         "name": "John",
@@ -40,7 +40,7 @@ You could optimize the code to split the search, but even with this, it still la
         "name": "Paul",
         "department": "House Floor"
     }
-}
+    }
 
 def get_employee_from_dict(id):
     return employee_dict[id];
@@ -50,8 +50,10 @@ print(get_employee_from_dict(12458));
 
 
 
-ZeroDivisionError
-In math there are rules about dividing by zero. The below code is trying to do just that and will throw a ZeroDivisionError. Add exception handling to return back 0 instead of allowing the error to be thrown.
+# ZeroDivisionError
+# In math there are rules about dividing by zero.
+# below code is trying to do just that and will throw a ZeroDivisionError.
+# exception handling to return back 0 instead of allowing the error to be thrown.
 
 # Starter code
 def divide_by(a, b):
@@ -62,8 +64,9 @@ except Exception as e:
     print("invalid",e)
 
 FileNotFoundError
-The code below is looking for a file that does not exist. Add exception handling to catch this error and return the following "The file could not be found".
-# Starter code
+# The code below is looking for a file that does not exist.
+# Add exception handling to catch this error and return the following "The file could not be found".
+# # Starter code
 try:
     with open('file_does_not_exist.txt', 'r') as file:
         print(file.read())
