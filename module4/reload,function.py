fd41adb36ea84b5b5f680f91738296753018d79b
AliMurad-codes <ranaali0601@gmail.com>
2024-08-21 21:22:26 +0500

    commit

---
+++ module4/reload,function.py
@@ -0,0 +1,12 @@
+# in reload funtion you can import a file many time as per ur need
+# However, I can change this with the help of the reload function.
+# Let me remove this code and add the importLib module where the reload function sits
+
+import importlib
+import reload1
+
+importlib.reload(reload1)
+importlib.reload(reload1)
+importlib.reload(reload1)
+
+
