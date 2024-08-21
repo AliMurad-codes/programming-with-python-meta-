572f6a7c7062fcf28656a1c74ee47d6235489ffa
AliMurad-codes <ranaali0601@gmail.com>
2024-08-21 15:45:02 +0500

    commit

--- module4/aceessmodule.py
+++ module4/aceessmodule.py
@@ -1,1 +1,17 @@
-dlcnldnl
+# import sys
+# locations = sys.path
+# print(locations)
+# for i in locations:
+#     print(i)
+#
+#
+# import calendar
+#
+# leapdays = calendar.leapdays(2000 , 2050)
+# print(leapdays)
+#
+# isit_leap = calendar.isleap(2036)
+# print(isit_leap)
+
+# we can import the other file in our main module for example:
+
