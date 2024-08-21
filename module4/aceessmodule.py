a05adf246c6bba5842b141d0ce63a1906d8a0c52
AliMurad-codes <ranaali0601@gmail.com>
2024-08-21 17:10:06 +0500

    commit

--- module4/aceessmodule.py
+++ module4/aceessmodule.py
@@ -51,3 +51,25 @@ root = factorial(10)
 print("The factorial of 9 is :" , root)
 soot = sqrt(9)
 print("The squareroot of 9 is :" , soot)
+
+
+# There are four main types of scopes that can be defined in Python: local, enclosed, global and built-in.
+# The practice of trying to determine in which scope a certain variable belongs is known as scope resolution.
+# Local, this is where the first search for a variable is in the local scope.
+# Enclosed, this is defined inside an enclosing or nested functions.
+# Global is defined at the uppermost level or simply outside functions,
+# and built-in,which is the keywords present in the built-in module.
+
+
+def a():
+    animal = "elephant"
+    def b():
+        nonlocal  animal
+        animal = "lion"
+        print("inside nested function: " + animal)
+    print("before calling  function: " +animal)
+    b()
+    print("after nested function:" + animal)
+animal = " camel"
+a()
+print("Global animal:" + animal)
